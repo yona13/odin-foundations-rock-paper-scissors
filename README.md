4c2f1bd3428217717edd969e69d84e2e03462966
Yona <yona.sandt@gmail.com>
2023-04-21 17:20:37 +0800

    Project complete

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # Odin Foundations Project: Rock, Paper Scissors
 Implementation of the classic game, "Rock, Paper, Scissors"
 
-## Console Game:
-/* Date: 21/04/23 Time: 11:45 */
+## Iteration 1: [Date: 21/04/23 Time: 11:45]
  - Current iteration is just a computer playing against itself twenty times
  - Each "round" returns a value between -1, 0, 1
+
+## Iteration 2: [Date: 21/04/23 Time: 17:15]
+ - On opening the page, via https://yona13.github.io/odin-foundations-rock-paper-scissors/, the user will see a welcome message along with a button, asking the user to play.
+ - The welcome message will tell the user to open the console.
+ - If the user presses play, they will be asked if they want to enter their name, and if so they will be prompted to enter it
+ - If entered, the welcome message will update, and the prompts will be stylised with the player's name
+ - The user will then be asked to enter rock, paper or scissors. The case doesn't matter, but spelling is important
+ - The user will then play the computer, who will flip a 3-sided coin to select it's option
+ - Best of 5. The first person to beat 3 games wins.
+ - The player can replay the game, if they so choose.
--- rps.js
+++ rps.js
@@ -3,6 +3,9 @@ let POINTERS = {
     DRAW: 0,
     COMPUTER_WIN: -1
 };
+let firstGame = true;
+const button = document.querySelector("button");
+const aName = document.querySelector("a");
 
 function getComputerChoice() {
     const i = Math.floor(Math.random() * 3);
@@ -24,16 +27,81 @@ function playRound(playerSelection, computerSelection) {
         return POINTERS.DRAW;
     } else {
         if ((pS == "rock" && cS == "scissors") || (pS == "paper" && cS == "rock") || (pS == "scissors" && cS == "paper")) {
-            console.log(`You Won! ${pS} beats ${cS}!`);    
+            console.log(`You did it! ${pS} beats ${cS}!`);    
             return POINTERS.PLAYER_WIN;
         } else {
-            console.log(`Dang, You Lost! ${cS} beats ${pS}!`);
+            console.log(`Dang, unlucky! ${cS} beats ${pS}!`);
             return POINTERS.COMPUTER_WIN;
         }
     }
 }
 
-for (var i = 1; i <= 20; i ++) {
-    console.log(`Round ${i}:`);
-    playRound(getComputerChoice(), getComputerChoice());
+function game() {
+    // Best of 5 Games
+    let pScore = 0;
+    let cScore = 0;
+    let firstRound = firstGame;
+    let pName = firstGame ? "Player" : aName.text
+
+    while (!(pScore == 3 || cScore == 3)) {
+        let choice;
+        if (firstRound) {
+            firstRound = false;
+            if (confirm("Welcome to Rock, Paper, Scissors. Would you like to enter your name?")) {
+                pName = prompt("Great, enter your name please.");
+                aName.textContent = pName;
+            }
+            choice = prompt(`Welcome ${pName}. Please enter your choice (rock, paper or scissors).`);
+        } else {
+            let preamble;
+            if (pScore > cScore)
+                preamble = `Well done ${pName}!`;
+            else if (pScore < cScore)
+                preamble = `Unlucky ${pName}.`;
+            else
+                preamble = "It's anyone's game!"
+            choice = prompt(`${preamble} The Score: ${pScore} vs ${cScore}; ${pName} vs Computer;. Please enter your next choice (rock, paper or scissors).`);
+        }
+        
+        if (choice == null)
+            return;
+        
+        let success = false;
+        let replay = false;
+        while (!success) {
+            if (replay) {
+                let newChoice = prompt(`You and the computer drew by both choosing ${choice}. Please choose again (rock, paper or scissors).`);
+                choice = newChoice;
+            } 
+            if (choice.toLowerCase() == "rock" || choice.toLowerCase() == "paper" || choice.toLowerCase() == "scissors") {
+                let score = playRound(choice, getComputerChoice());
+                if (score == 0) {
+                    replay = true;
+                    success = false;
+                } else if (score > 0) {
+                    pScore += 1;
+                    success = true;
+                } else {
+                    cScore += 1;
+                    success = true;
+                }
+            } else {
+                let newChoice = prompt(`Sorry, you need to enter one of the following: rock, paper or scissors.`);
+                choice = newChoice
+            }
+        }
+    }
+
+    if (cScore > pScore) {
+        console.log("Unlucky, seems like the computer has won. Better luck next time");
+    } else {
+        console.log("Well done! You beat the computer! I bet your mum's proud.");
+    }
+
+    if (firstGame) {
+        firstGame = false;
+        button.textContent = "Replay?"
+    }
 }
+
+button.addEventListener('click', game);
